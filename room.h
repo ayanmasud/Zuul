b0bf04ebc77b12e3396b2b4bc8a606e15206a945
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2024-11-12 12:52:53 -0800

    Update room.h

--- room.h
+++ room.h
@@ -5,6 +5,7 @@ using namespace std;
 
 class room {
  public:
+  char name[80];
   char description[80];
   bool hasItem1 = false;
   bool hasItem2 = false;
@@ -12,9 +13,9 @@ class room {
   bool hasItem4 = false;
   bool hasItem5 = false;
 
-  map<char, room> exits;
+  map<char, room*> exits;
 
   room();
-  void setExit(char direction[6], room neighbor);
+  void setExit(char direction, room* neighbor);
   void getDescription();
 };
