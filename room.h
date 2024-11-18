b3ee97d3f6a0f18bb8f55d9b616e6cbbc887a305
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2024-11-18 13:13:13 -0800

    Update room.h

--- room.h
+++ room.h
@@ -3,17 +3,18 @@
 
 using namespace std;
 
+// this file carries each of the variables that room types have
 class room {
  public:
-  char name[80];
-  char description[80];
-  bool hasItem1 = false;
-  bool hasItem2 = false;
-  bool hasItem3 = false;
-  bool hasItem4 = false;
-  bool hasItem5 = false;
+  char name[80]; // room name
+  char description[80]; // room description
+  bool hasItem1 = false; // spoon
+  bool hasItem2 = false; // toilet paper
+  bool hasItem3 = false; // photo of ehan
+  bool hasItem4 = false; // phone
+  bool hasItem5 = false; // car keys
 
-  map<char, room*> exits;
+  map<char, room*> exits; // map contains all the possible exits for this room. stored by the direction (char) and the neighbor (room)
 
   room();
   void setExit(char direction, room* neighbor);
